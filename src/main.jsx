778af56d7c496c0579ba45ec8322d5554b27fd68
Juan Pablo Anuard Vazquez Guzman <juanpablovazques544@gmail.com>
2026-05-06 21:56:21 -0600

    clean input cuando es cero

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// Limpia el input automáticamente cuando el valor es "0" y se hace foco (solo type="number")
+document.addEventListener('focusin', (e) => {
+  const el = e.target
+  if (el.tagName === 'INPUT' && el.type === 'number' && el.value.trim() === '0') {
+    el.value = ''
+    el.dispatchEvent(new Event('input', { bubbles: true }))
+    el.dispatchEvent(new Event('change', { bubbles: true }))
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
